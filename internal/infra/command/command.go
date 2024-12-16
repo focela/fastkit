95015c367ebea04c53af9de69698eabf407d30c2
Son Tran Thanh <286.trants@gmail.com>
2024-12-17 00:35:11 +0700

    feat(command): add Init and parsing utilities for CLI arguments
    Introduce the `Init` function to initialize and parse default arguments
    and options for command-line utilities. Added the
    `parseUsingDefaultAlgorithm` function for argument parsing logic,
    utilizing regex to separate options and arguments.

--- internal/infra/command/command.go
+++ internal/infra/command/command.go
@@ -14,7 +14,9 @@
 package command
 
 import (
+	"os"
 	"regexp"
+	"strings"
 )
 
 // Global variables for storing parsed arguments and options.
@@ -23,3 +25,45 @@ var (
 	defaultParsedOptions map[string]string
 	argumentOptionRegex  = regexp.MustCompile(`^\-{1,2}([\w\?\.\-]+)(=)?(.*)$`)
 )
+
+// Init initializes defaultParsedArgs and defaultParsedOptions.
+func Init(args ...string) {
+	if len(args) == 0 {
+		if len(defaultParsedArgs) == 0 && len(defaultParsedOptions) == 0 {
+			args = os.Args
+		} else {
+			return
+		}
+	} else {
+		defaultParsedArgs = []string{}
+		defaultParsedOptions = map[string]string{}
+	}
+	// Parse arguments using the default algorithm.
+	defaultParsedArgs, defaultParsedOptions = parseUsingDefaultAlgorithm(args...)
+}
+
+// parseUsingDefaultAlgorithm parses command-line arguments into args and options.
+func parseUsingDefaultAlgorithm(args ...string) ([]string, map[string]string) {
+	parsedArgs := []string{}
+	parsedOptions := make(map[string]string)
+
+	for i := 0; i < len(args); {
+		matches := argumentOptionRegex.FindStringSubmatch(args[i])
+		if len(matches) > 2 {
+			key := matches[1]
+			if matches[2] == "=" {
+				parsedOptions[key] = matches[3]
+			} else if i < len(args)-1 && !strings.HasPrefix(args[i+1], "-") {
+				parsedOptions[key] = args[i+1]
+				i += 2
+				continue
+			} else {
+				parsedOptions[key] = matches[3]
+			}
+		} else {
+			parsedArgs = append(parsedArgs, args[i])
+		}
+		i++
+	}
+	return parsedArgs, parsedOptions
+}
