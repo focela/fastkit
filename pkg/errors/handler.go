876733370a5c5c97e7a31d39e1a381ac01339bbd
Son Tran Thanh <286.trants@gmail.com>
2024-12-27 00:05:53 +0700

    feat(errors): add error handling methods
    Added error handling methods (`Error`, `Cause`, `Current`, `Unwrap`) in the `errors` package. These methods provide enhanced functionalities for error messaging, root cause retrieval, current error state, and error unwrapping.

--- pkg/errors/handler.go
+++ pkg/errors/handler.go
@@ -6,6 +6,7 @@
 package errors
 
 import (
+	"errors"
 	"runtime"
 	"strings"
 
@@ -34,4 +35,71 @@ func init() {
 	if goRootForFilter != "" {
 		goRootForFilter = strings.ReplaceAll(goRootForFilter, "\\", "/")
 	}
+}
+
+// Error returns the error message as a string.
+// If both `text` and `code` are empty, it returns the wrapped error's message.
+func (err *Error) Error() string {
+	if err == nil {
+		return ""
+	}
+	errStr := err.text
+	if errStr == "" && err.code != nil {
+		errStr = err.code.Message()
+	}
+	if err.error != nil {
+		if errStr != "" {
+			errStr += ": "
+		}
+		errStr += err.error.Error()
+	}
+	return errStr
+}
+
+// Cause retrieves the root cause of the error.
+// It unwraps the error recursively until the base cause is found.
+func (err *Error) Cause() error {
+	if err == nil {
+		return nil
+	}
+	loop := err
+	for loop != nil {
+		if loop.error != nil {
+			if e, ok := loop.error.(*Error); ok {
+				// Internal Error struct.
+				loop = e
+			} else if e, ok := loop.error.(Causer); ok {
+				// External error implementing the Causer interface.
+				return e.Cause()
+			} else {
+				return loop.error
+			}
+		} else {
+			return errors.New(loop.text)
+		}
+	}
+	return nil
+}
+
+// Current returns a copy of the current error instance.
+// If the error is nil, it returns nil.
+func (err *Error) Current() error {
+	if err == nil {
+		return nil
+	}
+	return &Error{
+		error: nil,
+		stack: err.stack,
+		text:  err.text,
+		code:  err.code,
+	}
+}
+
+// Unwrap returns the next error in the error chain.
+// It is compatible with Go's standard library errors.Unwrap.
+func (err *Error) Unwrap() error {
+	if err == nil {
+		return nil
+	}
+	return err.error
 }
