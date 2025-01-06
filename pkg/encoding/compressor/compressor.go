bc73a74fcc9f522bebce8f18c8843cb8e3684263
Son Tran Thanh <286.trants@gmail.com>
2025-01-06 22:41:05 +0700

    feat(encoding/compressor): add compression algorithms
    Add initial implementation of the `compressor` package, providing
    various compression algorithms for binary and byte data.

---
+++ pkg/encoding/compressor/compressor.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+// Internal use only. Unauthorized use is prohibited.
+// Contact: legal@focela.com
+
+// Package compressor provides various compression algorithms for binary and byte data.
+package compressor
