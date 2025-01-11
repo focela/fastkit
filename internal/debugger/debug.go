b34a7a47ec9021fe3c95644d40a08d3596cd93a5
Son Tran Thanh <286.trants@gmail.com>
2025-01-11 21:11:20 +0700

    feat(debugger): add utilities for logging and application state tracking
    Introduce the internal/debugger/debug.go file to provide debugging utilities for the Aegis framework.
    This file includes:
    - Core utilities for logging.
    - Functions to track and inspect application state during runtime.
    
    These additions enhance the framework's debugging capabilities, making it easier to diagnose issues and monitor application behavior.

---
+++ internal/debugger/debug.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2024 Focela Technologies. All rights reserved.
+// Internal use only. Unauthorized use is prohibited.
+// Contact: legal@focela.com
+
+// Package debugger provides utilities for debugging, including logging and tracking application state.
+package debugger
